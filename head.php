5f87641d99851bebf19c50760c9ee4a6b1e74e10
piyush734 <piyushrathi734@gmail.com>
2017-06-23 10:43:03 +0530

    Add files via upload

---
+++ head.php
@@ -0,0 +1,28 @@
+
+        <meta name="author" content="piyush rathi">
+        <meta name="application-name" content="cab booking">
+        
+        <title>cab ride</title>   
+          
+        <!-- Latest compiled and minified CSS -->
+
+
+   <link href="css/materialize.css" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+       <link href="css/style.css" rel="stylesheet">
+       <link href="https://fonts.googleapis.com/css?family=Droid+Serif|Overpass" rel="stylesheet">
+        
+        <script src="js/materialize.js"></script>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+    
+   
+</script>
